62f475e84d9a7050af3189694defff8521468c7d
Emily <847432003@qq.com>
2019-03-17 13:47:23 +0800

    Update unicode utf8 http

---
+++ LinqX.Core/HttpFactory/HttpRequestClient.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqX.Core.HttpFactory
+{
+    public class HttpRequestClient
+    {
+        /// <summary>
+        /// 将数据制作表单数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="Entity"></param>
+        /// <param name="Map"></param>
+        /// <returns></returns>
+        public static IList<KeyValuePair<String, String>> KeyValuePairs<T>(T Entity, IDictionary<string, string> Map = null) where T : class, new()
+        {
+            IList<KeyValuePair<String, String>> keyValuePairs = new List<KeyValuePair<string, string>>();
+            Entity.GetType().GetProperties().ToList().ForEach(t =>
+            {
+                var flag = t.CustomAttributes.Where(x => x.AttributeType == typeof(JsonIgnoreAttribute)).FirstOrDefault();
+                if (Map != null)
+                    foreach (KeyValuePair<String, String> item in Map)
+                    {
+                        if (item.Key.Equals(t.Name))
+                            keyValuePairs.Add(new KeyValuePair<string, string>(item.Value, t.GetValue(Entity).ToString()));
+                    }
+                else if (flag == null)
+                    keyValuePairs.Add(new KeyValuePair<string, string>(t.Name, t.GetValue(Entity).ToString()));
+            });
+            return keyValuePairs;
+        }
+        /// <summary>
+        /// Post异步请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="data"></param>
+        /// <param name="headers"></param>
+        /// <param name="contentType"></param>
+        /// <param name="timeout"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static async Task<String> HttpPostAsync(string url, IList<KeyValuePair<String, String>> data, Dictionary<string, string> headers = null, string contentType = null, int timeout = 0, Encoding encoding = null)
+        {
+            using (HttpClient Client = new HttpClient())
+            {
+                if (headers != null)
+                    foreach (KeyValuePair<string, string> header in headers)
+                    {
+                        Client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                    }
+                if (timeout > 0)
+                    Client.Timeout = new TimeSpan(0, 0, timeout);
+                HttpContent content = new FormUrlEncodedContent(data);
+                if (contentType != null)
+                    content.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+                Client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36");
+                HttpResponseMessage responseMessage = await Client.PostAsync(url, content);
+                Byte[] resultBytes = await responseMessage.Content.ReadAsByteArrayAsync();
+                return Encoding.UTF8.GetString(resultBytes);
+            }
+        }
+        /// <summary>
+        /// Get异步请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="headers"></param>
+        /// <param name="timeout"></param>
+        /// <returns></returns>
+        public static async Task<String> HttpGetAsync(string url, Dictionary<string, string> headers = null, int timeout = 0)
+        {
+            using (HttpClient Client = new HttpClient())
+            {
+                if (headers != null)
+                    foreach (KeyValuePair<string, string> header in headers)
+                    {
+                        Client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                    }
+                if (timeout > 0)
+                    Client.Timeout = new TimeSpan(0, 0, timeout);
+                Client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36");
+                Byte[] resultBytes = await Client.GetByteArrayAsync(url);
+                return Encoding.Default.GetString(resultBytes);
+            }
+        }
+    }
+}
--- LinqX.Core/XCore/LinqX.cs
+++ LinqX.Core/XCore/LinqX.cs
@@ -5,12 +5,45 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace LinqX.Core.XCore
 {
     public static class LinqX
     {
-        public static List<String> ToNames<T>(this T Param)
+        public static T ToUTF8<T>(this T Param) where T : class
+        {
+            Param.GetType().GetProperties().ToList().ForEach(t =>
+            {
+                var data = t.GetValue(Param).ToString();
+                if (!string.IsNullOrEmpty(data))
+                {
+                    var bytes = Encoding.Unicode.GetBytes(data);
+                    StringBuilder str = new StringBuilder();
+                    for (int i = 0; i < bytes.Length; i += 2)
+                    {
+                        str.AppendFormat("u{0}{1}", bytes[i + 1].ToString("x").PadLeft(2, '0'), bytes[i].ToString("x").PadLeft(2, '0'));
+                    }
+                    t.SetValue(Param, str.ToString());
+                }
+            });
+            return Param;
+        }
+        public static T ToUnic<T>(this T Param) where T : class
+        {
+            Param.GetType().GetProperties().ToList().ForEach(t =>
+            {
+                var data = t.GetValue(Param).ToString();
+                if (!string.IsNullOrEmpty(data))
+                {
+                    var result = new Regex(@"\\u([0-9A-F]{4})", RegexOptions.IgnoreCase | RegexOptions.Compiled)
+                    .Replace(data, x => string.Empty + Convert.ToChar(Convert.ToUInt16(x.Result("$1"), 16)));
+                    t.SetValue(Param, result);
+                }
+            });
+            return Param;
+        }
+        public static List<String> ToNames<T>(this T Param) where T : class
         {
             List<String> Names = new List<String>();
             Param.GetType().GetProperties().ToList().ForEach(t =>
@@ -19,7 +52,7 @@ namespace LinqX.Core.XCore
             });
             return Names;
         }
-        public static List<Object> ToValues<T>(this T Param)
+        public static List<Object> ToValues<T>(this T Param) where T : class
         {
             List<Object> Values = new List<Object>();
             Param.GetType().GetProperties().ToList().ForEach(t =>
@@ -28,7 +61,7 @@ namespace LinqX.Core.XCore
             });
             return Values;
         }
-        public static IDictionary<String, Object> ToDic<T>(this T Param)
+        public static IDictionary<String, Object> ToDic<T>(this T Param) where T : class
         {
             ParameterExpression Parameter = Expression.Parameter(Param.GetType(), "t");
             Dictionary<String, Object> Map = new Dictionary<String, Object>();
@@ -41,19 +74,19 @@ namespace LinqX.Core.XCore
             });
             return Map;
         }
-        public static String ToDes<T>(this T Param, Expression<Func<T, object>> Express)
+        public static String ToDes<T>(this T Param, Expression<Func<T, object>> Express) where T : class
         {
             MemberExpression Exp = (MemberExpression)Express.Body;
             var Obj = Exp.Member.GetCustomAttributes(typeof(DescriptionAttribute), true).FirstOrDefault();
             return (Obj as DescriptionAttribute).Description;
         }
-        public static Int64 ToLong<T>(this T Param, Expression<Func<T, object>> Express)
+        public static Int64 ToLong<T>(this T Param, Expression<Func<T, object>> Express) where T : class
         {
             String Str = ((Express.Body as MemberExpression).Member as PropertyInfo).GetValue(Param).ToString();
             Int64.TryParse(Str, out Int64 Value);
             return Value;
         }
-        public static void ToSet<T>(this T Param, Expression<Func<T, object>> Expres, Object Value)
+        public static void ToSet<T>(this T Param, Expression<Func<T, object>> Expres, Object Value) where T : class
         {
             var Property = ((Expres.Body as MemberExpression).Member as PropertyInfo);
 
