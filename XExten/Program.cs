5cbbbd4a8c638a73f47be91ab3206403ee87be86
Emily <847432003@qq.com>
2019-03-18 23:22:27 +0800

    Remove demo

--- XExten/Program.cs
+++ XExten/Program.cs
@@ -9,25 +9,8 @@ namespace XExten
     {
         public static void Main(string[] args)
         {
-            List<TestA> ta = new List<TestA>();
-            TestA t1 = new TestA();
-            t1.Id = 1;
-            t1.Name = "2";
-           var pp1= t1.ByMap<TestA, TestB>();
-            ta.Add(t1);
-            var pp = ta.ByMaps<TestA,TestB>();
+
         }
 
-    }
-    public class TestA
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-
-    public class TestB
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
     }
 }
