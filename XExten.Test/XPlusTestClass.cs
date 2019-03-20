84c5480681474e69dec5f4f6496e9d98d60245bf
Emily <847432003@qq.com>
2019-03-20 13:09:54 +0800

    think 1.0.2 dev

---
+++ XExten.Test/XPlusTestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XExten.XPlus;
+using Xunit;
+
+namespace XExten.Test
+{
+    public class XPlusTestClass
+    {
+        [Fact]
+        public void XTel_Test()
+        {
+            XPlusEx.XTel();
+        }
+    }
+}
---
+++ XExten/XPlus/XPlusEx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XExten.XPlus
+{
+    public class XPlusEx
+    {
+        #region Func
+        #endregion
+
+        #region Sync
+        /// <summary>
+        /// return a Random Tel
+        /// </summary>
+        /// <returns></returns>
+        public static String XTel()
+        {
+            String[] PhonesHost = "134,135,136,137,138,139,150,151,152,157,158,159,130,131,132,155,156,133,153,180,181,182,183,185,186,176,187,188,189,177,178".Split(',');
+            Random random = new Random();
+            int index = random.Next(0, PhonesHost.Length - 1);
+            return PhonesHost[index] + (random.Next(100, 888) + 10000).ToString().Substring(1) + (random.Next(1, 9100) + 10000).ToString().Substring(1);
+        }
+        #endregion
+
+        #region ASync
+        /// <summary>
+        /// return a Random Tel
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<String> XTelAsync()
+        {
+            return await Task.Run(() => XTel());
+        }
+        #endregion
+    }
+}
