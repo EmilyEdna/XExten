e34d88f92bf521ef8c5a428cfef0407c39fb81a1
Emily <847432003@qq.com>
2019-04-28 11:06:25 +0800

    add easy queue

---
+++ XExten.Test/QueueTestClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XExten.MessageQueue;
+using Xunit;
+
+namespace XExten.Test
+{
+    public class QueueTestClass
+    {
+        [Fact]
+        public void QueueTest()
+        {
+            XQueue mq = new XQueue();
+            var count = mq.GetCount();
+            var key = Guid.NewGuid().ToString();
+            //Test Add()
+            var item = mq.Add(key, () => Console.WriteLine("测试写入"));
+            Assert.Equal(count + 1, mq.GetCount());
+            //Test GetCurrentKey()
+            var currentKey = mq.GetCurrentKey();
+            Assert.Equal(key, currentKey);
+            //Test GetItem
+            var currentItem = mq.GetItem(currentKey);
+            Assert.Equal(currentItem.Key, item.Key);
+            Assert.Equal(currentItem.AddTime, item.AddTime);
+            //Test Remove
+            mq.Remove(key);
+            Assert.Equal(count, mq.GetCount());
+        }
+    }
+}
---
+++ XExten/MessageQueue/QueueDictionary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XExten.MessageQueue
+{
+    public class QueueDictionary : Dictionary<String, QueueItem>
+    {
+        public QueueDictionary() : base(StringComparer.OrdinalIgnoreCase){ }
+    }
+}
