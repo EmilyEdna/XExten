8de9e43ae9496109de5be654c4f4160891dc0820
EmilyEdna <847432003@qq.com>
2019-06-25 15:06:37 +0800

    Update LinqX.cs

--- XExten/XCore/LinqX.cs
+++ XExten/XCore/LinqX.cs
@@ -394,7 +394,7 @@ namespace XExten.XCore
             {
                 Total = queryable.Count(),
                 TotalPage = (int)Math.Ceiling(queryable.Count() / (double)PageSize),
-                CurrentPage = (int)Math.Ceiling(PageIndex / (double)PageSize) + 1,
+                CurrentPage = (int)Math.Ceiling(PageIndex / (double)PageSize),
                 Queryable = queryable.Skip((PageIndex - 1) * PageSize).Take(PageSize).AsQueryable()
             };
         }
