a66fbdd40ed85d18a9f579a3e794d32a8bc2a994
EmilyEdna <847432003@qq.com>
2020-02-22 13:37:23 +0800

    fix HttpKeyPairs

--- XExten/HttpFactory/HttpKeyPairs.cs
+++ XExten/HttpFactory/HttpKeyPairs.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using XExten.XCore;
 
 namespace XExten.HttpFactory
 {
@@ -26,15 +28,29 @@ namespace XExten.HttpFactory
             {
                 var flag = t.CustomAttributes.Where(x => x.AttributeType == typeof(JsonIgnoreAttribute)).FirstOrDefault();
                 if (Map != null)
-                    foreach (KeyValuePair<String, String> item in Map)
+                {
+                    if (!Map.Keys.Where(Item => Item.Equals(t.Name)).FirstOrDefault().IsNullOrEmpty())
+                        keyValuePairs.Add(new KeyValuePair<string, string>(Map[t.Name], RetrunEnumValue(t, Entity)));
+                    else
                     {
-                        if (item.Key.Equals(t.Name))
-                            keyValuePairs.Add(new KeyValuePair<string, string>(item.Value, t.GetValue(Entity).ToString()));
+                        keyValuePairs.Add(new KeyValuePair<string, string>(t.Name, RetrunEnumValue(t, Entity)));
                     }
+                }
                 else if (flag == null)
-                    keyValuePairs.Add(new KeyValuePair<string, string>(t.Name, t.GetValue(Entity).ToString()));
+                    keyValuePairs.Add(new KeyValuePair<string, string>(t.Name, RetrunEnumValue(t, Entity)));
             });
             return keyValuePairs;
+        }
+        /// <summary>
+        /// 返回枚举值
+        /// </summary>
+        /// <returns></returns>
+        private static string RetrunEnumValue<T>(PropertyInfo Info, T Entity)
+        {
+            if (Info.PropertyType.BaseType == typeof(Enum))
+                return ((int)Enum.Parse(Info.PropertyType, Info.GetValue(Entity).ToString())).ToString();
+            else
+                return Info.GetValue(Entity).ToString();
         }
     }
 }
